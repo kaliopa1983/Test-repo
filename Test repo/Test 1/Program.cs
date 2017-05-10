e12283c003a3f46b656c41d5c3e1eb1b761503da
debora <d_kalcheva@abv.bg>
2017-05-10 09:51:03 +0300

    Add VS text

---
+++ Test repo/Test 1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+
+namespace Test_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Git Hub ! ");
+        }
+    }
+}
